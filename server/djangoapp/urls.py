c3482f8d1171e6c337cd3b6295a6abe8c365f040
mcqueecs <mcqueecs@mail.uc.edu>
2026-04-26 20:21:18 -0400

    Task 3.2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,12 +7,13 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-   path(route='registration', view=views.registration, name='registration'),
+    path(route='registration', view=views.registration, name='registration'),
 
     # path for login
-   path(route='login', view=views.login_user, name='login'),
-   path(route='logout', view=views.logout_request, name='logout'),
+    path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
 
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
